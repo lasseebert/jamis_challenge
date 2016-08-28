970dc0664c6d195a01444c7912155581e45017e8
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2016-08-28 21:39:20 +0200

    Implemented priority queue using the heap, 005

---
+++ 005_heap/lib/heap/queue.ex
@@ -0,0 +1,27 @@
+defmodule Heap.Queue do
+  @moduledoc """
+  A priority queue implemented with a binary heap
+  """
+
+  def new do
+    Heap.new(fn {priority, _item} -> priority end)
+  end
+
+  def enqueue(queue, priority, item) do
+    Heap.insert(queue, {priority, item})
+  end
+
+  def dequeue(queue) do
+    {{_priority, item}, queue} = Heap.remove(queue)
+    {item, queue}
+  end
+
+  def size(queue), do: Heap.size(queue)
+
+  def height(queue), do: Heap.height(queue)
+
+  def peak(queue) do
+    {_priority, item} = Heap.peak(queue)
+    item
+  end
+end
---
+++ 005_heap/test/queue_test.exs
@@ -0,0 +1,22 @@
+defmodule Heap.QueueTest do
+  use ExUnit.Case
+
+  alias Heap.Queue
+
+  test "using default comparison" do
+    queue =
+      Queue.new
+      |> Queue.enqueue(3, "kiwi")
+      |> Queue.enqueue(1, "apple")
+      |> Queue.enqueue(2, "banana")
+
+    {v1, queue} = Queue.dequeue(queue)
+    {v2, queue} = Queue.dequeue(queue)
+    {v3, queue} = Queue.dequeue(queue)
+
+    assert v1 == "apple"
+    assert v2 == "banana"
+    assert v3 == "kiwi"
+    assert Queue.size(queue) == 0
+  end
+end
