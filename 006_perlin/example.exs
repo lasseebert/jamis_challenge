3790ba8303b841079309d40bfcce690b30087c1f
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2016-09-10 00:01:48 +0200

    Easier to read example in README, 006

--- 006_perlin/example.exs
+++ 006_perlin/example.exs
@@ -9,21 +9,18 @@ raw = Raw.new(width, height)
 image_data =
   Perlin.generate({width, height}, 2, 5, 0.7)
   |> Enum.reduce(raw, fn
-    {{x, y}, value}, raw ->
+    {point, value}, raw ->
       value = (value + 1) / 2
 
-      r0 = 0
-      g0 = 0
-      b0 = 153
+      {r0, g0, b0} = {0, 0, 153}
+      {r1, g1, b1} = {0, 255, 0}
 
-      r1 = 0
-      g1 = 255
-      b1 = 0
-
-      r = (r1 - r0) * value + r0 |> trunc
-      g = (g1 - g0) * value + g0 |> trunc
-      b = (b1 - b0) * value + b0 |> trunc
-      Raw.draw_pixel(raw, {x, y}, {r, g, b})
+      rgb_pixel = {
+        (r1 - r0) * value + r0 |> trunc,
+        (g1 - g0) * value + g0 |> trunc,
+        (b1 - b0) * value + b0 |> trunc
+      }
+      Raw.draw_pixel(raw, point, rgb_pixel)
   end)
 |> PPMRenderer.to_binary
 
